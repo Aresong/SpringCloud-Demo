6888ba35b3e3357e865cd1fdc4b58edd98ff260e
Aresong <szhzzzzz@163.com>
2019-08-26 22:03:33 +0800

    20190826 config client

---
+++ microservice-config-client-3355/src/main/java/com/s5/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package com.s5.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Aresong
+ * @Date: 2019/8/26 21:58
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
+    }
+}
---
+++ microservice-config-client-3355/src/main/java/com/s5/springcloud/rest/ConfigClientRest.java
@@ -0,0 +1,32 @@
+package com.s5.springcloud.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ /**
+ * @Author: Aresong
+ * @Date: 2019/8/26 21:58
+ * @Version 1.0
+ */
+@RestController
+public class ConfigClientRest {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Value("${eureka.client.server-url.defaultZone}")
+    private String eurekaServers;
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig(){
+
+        System.out.println("applicationName:"+this.applicationName
+                +"eurekaServers:"+this.eurekaServers
+                +"port:"+this.port);
+        return "applicationName:"+this.applicationName
+                +"eurekaServers:"+this.eurekaServers
+                +"port:"+this.port;
+    }
+}
